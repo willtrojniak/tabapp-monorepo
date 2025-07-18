abd127a6d94082aedf99230ce3533a9a54dd32c5
Will Trojniak <william.trojniak@gmail.com>
2025-07-17 23:58:46 -0700

    refactor: 🪜 move different parts of shop config to tabs

---
+++ src/routes/_auth/shops/$shopId/_layout.tsx
@@ -0,0 +1,36 @@
+import { NavigationMenu, NavigationMenuLink } from '@/components/ui/navigation-menu'
+import { } from '@/components/ui/tabs'
+import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/_auth/shops/$shopId/_layout')({
+  component: ShopIndexLayoutComponent,
+  beforeLoad: () => ({ title: "" })
+})
+
+function ShopIndexLayoutComponent() {
+  const { shopId } = Route.useParams()
+
+  return <div>
+    <NavigationMenu className='bg-muted px-1 py-1 mb-2 rounded-md'>
+      <NavigationMenuLink asChild>
+        <Link to='/shops/$shopId' params={{ shopId }}
+          className='px-2 py-1 rounded-sm hover:cursor-pointer underline-offset-2' activeProps={{ className: "bg-background" }} activeOptions={{ exact: true }}>
+          Shop
+        </Link>
+      </NavigationMenuLink>
+      <NavigationMenuLink asChild>
+        <Link to='/shops/$shopId/locations' params={{ shopId }}
+          className='px-2 py-1 rounded-sm hover:cursor-pointer underline-offset-2' activeProps={{ className: "bg-background" }} activeOptions={{ exact: true }}>
+          Locations
+        </Link>
+      </NavigationMenuLink>
+      <NavigationMenuLink asChild>
+        <Link to='/shops/$shopId/users' params={{ shopId }}
+          className='px-2 py-1 rounded-sm hover:cursor-pointer underline-offset-2' activeProps={{ className: "bg-background" }} activeOptions={{ exact: true }}>
+          Users
+        </Link>
+      </NavigationMenuLink>
+    </NavigationMenu>
+    <Outlet />
+  </div>
+}
